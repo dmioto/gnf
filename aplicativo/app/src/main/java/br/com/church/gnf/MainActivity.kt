63882ded89c3a0770414c7ee1f3a81cc513fad83
Daniel D. Mioto <daniel.mioto@outlook.com>
2020-04-05 13:44:31 -0300

    btn logout for tests

--- aplicativo/app/src/main/java/br/com/church/gnf/MainActivity.kt
+++ aplicativo/app/src/main/java/br/com/church/gnf/MainActivity.kt
@@ -1,12 +1,25 @@
 package br.com.church.gnf
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        logOut()
+    }
+
+    private fun logOut(){
+        logout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
